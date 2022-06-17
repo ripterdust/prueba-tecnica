9daee6598de0f9d02efea0beba981b394c2de8da
ripterdust <bryantello2010@hotmail.com>
2022-06-17 10:16:14 -0600

    Added the routing

--- src/js/controller/home.controller.ts
+++ src/js/controller/home.controller.ts
@@ -1,5 +1,12 @@
 import view from '../views/home.html';
 
+/*
+  Home view
+
+  @param content : HTMLDivElement
+
+*/
+
 export default (content: HTMLDivElement) => {
   const div = content;
   div.innerHTML = view;
