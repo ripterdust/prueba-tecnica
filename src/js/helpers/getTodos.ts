44cdcb5391dfef1bfadae77af3eeeea6dd22cd2e
ripterdust <bryantello2010@hotmail.com>
2022-06-17 11:48:34 -0600

    Fixed the width bug

--- src/js/helpers/getTodos.ts
+++ src/js/helpers/getTodos.ts
@@ -20,7 +20,9 @@ export const getTodos = (content: HTMLDivElement) => {
         <div class="todo box" id="todo" idTodo="${todo.id}">
             <div class="title" todoId="${todo.id}" id="todo-${todo.id}">${todo.name}</div>
             <div class="description">${todo.description}</div>
-            <button class="button deleteTodo" idTodo="${todo.id}"></button>
+            <button class="button deleteTodo" idTodo="${todo.id}">
+                <i class="fa-solid fa-trash-can"></i>
+            </button>
         </div>
     `;
     return true;
