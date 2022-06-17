fb124823fc9f43060601b6e78dd33646f8230624
ripterdust <bryantello2010@hotmail.com>
2022-06-17 11:36:37 -0600

    Added the todos styles

--- src/js/helpers/getTodos.ts
+++ src/js/helpers/getTodos.ts
@@ -17,7 +17,7 @@ export const getTodos = (content: HTMLDivElement) => {
   todos.innerHTML = '';
   data.map((todo: TodoModel) => {
     todos.innerHTML += `
-        <div class="todo" id="todo" idTodo="${todo.id}">
+        <div class="todo box" id="todo" idTodo="${todo.id}">
             <div class="title" todoId="${todo.id}" id="todo-${todo.id}">${todo.name}</div>
             <div class="description">${todo.description}</div>
             <button class="button deleteTodo" idTodo="${todo.id}"></button>
